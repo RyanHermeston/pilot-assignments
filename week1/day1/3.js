6e43df25c217dca554af3a61c9be0ac7f3cdeff4
Ryan <rwhermeston@uh.edu>
2016-08-31 17:19:08 -0500

    No more double quotes

--- week1/day1/3.js
+++ week1/day1/3.js
@@ -1,15 +1,15 @@
 var console = require('./lib/consolelog.js');
 
 module.exports = function chessboard() {
-  var str = "";
+  var str = '';
 var size =8;
 for(var i =0;i<size;i++){
 
   for(var j = 0;j<size/2;j++){
-    str+='#'+" ";
-  }str+="\n";
+    str+='#'+' ';
+  }str+='\n';
   if(i%2===0)
-    str+=" ";
+    str+=' ';
 }
 console.log(str);
 
