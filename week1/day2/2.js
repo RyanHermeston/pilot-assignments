7ea07fd1de5b3b1a0eb82b56595db1cc5ad2498f
RyanHermeston <ryan.hermeston@gmail.com>
2016-09-04 02:39:45 -0400

    prime number with loops

--- week1/day2/2.js
+++ week1/day2/2.js
@@ -1,12 +1,17 @@
 
 module.exports = function prime(n){
-  if(n <= 1)
-    return false;
-  if( n === 2 || n === 3 || n === 5 )
-        return true;
-  if( n % 2 === 0 || n % 3 === 0 || n % 5 === 0 || n % 7 === 0 )
-    return false;
-  else {
-    return true;
-  }
+	if (n < 2)
+		return false;
+	if(n === 2 || n === 3)
+		return true;
+	if(n % 2 === 0)
+		return false;
+	for(var i = 3; i<Math.sqrt(n)+1;i+=2){
+		if(n % i === 0){
+			return false;
+		}
+	}
+	return true;
+
+
 }
